e8a706830d10c6f970a40503f5c85a17fc7bf16b
pravin817 <83346334+pravin817@users.noreply.github.com>
2023-09-22 18:50:19 +0530

    Time: 4 ms (10.63%) | Memory: 7.4 MB (6.80%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -27,7 +27,49 @@ public:
         Space Complexity : O(1)
     */
 
+
+    bool solution2(string s, string t){
+        int m = s.length();
+        int n = t.length();
+
+        // create the vector to store the char with its idx 
+        map<char,vector<int>>mp;
+
+        for(int i = 0;i<n;i++){
+            mp[t[i]].push_back(i);
+        }
+
+        int prev = -1;
+
+        // now iterate through the s
+        for(int i = 0;i<m;i++){
+
+            char ch = s[i];
+
+            if(mp.find(ch)==mp.end()){
+                return false;
+            }
+
+            vector<int>indices = mp[ch];
+
+            auto it = upper_bound(begin(indices),end(indices),prev);   // log(n)
+
+            if(it == indices.end()){
+                return false;
+            }
+
+            prev = *it;
+        }
+        return true;
+    }
+
+    /*
+        Analysis:
+        Time Complexity : O(mlogn)
+        Space Complexity : O(1)
+    */    
+
     bool isSubsequence(string s, string t) {
-        return solution1(s,t);
+        return solution2(s,t);
     }
 };
