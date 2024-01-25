071ce4bd64a545f5b5b5d973a8cebc609bf00b9b
pravin817 <83346334+pravin817@users.noreply.github.com>
2024-01-25 23:27:24 +0530

    Time: 20 ms (90.94%) | Memory: 7.6 MB (87.95%) - LeetSync

---
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+    int SolveTab(string a, string b){
+        vector<int>curr(b.length()+1, 0);
+        vector<int>next(b.length()+1,0);
+        
+        for(int i = a.length()-1;i>=0;i--){
+            for(int j = b.length()-1;j>=0;j--){
+                int ans = 0;
+                if(a[i] == b[j]){
+                    ans = 1 + next[j+1];
+                }
+                else{
+                    ans = max(next[j],curr[j+1]);
+                }
+                curr[j] = ans;
+            }
+            next = curr;
+        }
+        return next[0];
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        return SolveTab(text1,text2);
+    }
+};
