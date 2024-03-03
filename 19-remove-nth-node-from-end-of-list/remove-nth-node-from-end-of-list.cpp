29af7faf4b0a63bf8d06434ac33d8a4576684b4b
pravin817 <83346334+pravin817@users.noreply.github.com>
2024-03-04 00:19:03 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (14.37%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -1,3 +1,62 @@
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+
+//     int getLengthOfLinkedList(ListNode *head){
+//         int len = 0;
+//         while(head!=NULL){
+//             len++;
+//             head = head->next;
+//         }
+
+//         return len;
+//     }
+
+
+//     ListNode* removeNthFromEnd(ListNode* head, int n) {
+//         int len = getLengthOfLinkedList(head);
+
+//         if(len == n){
+//             ListNode *temp = head->next;
+//             delete (head);
+//             return temp;
+//         }
+
+//         ListNode * temp = head;
+//         ListNode *prev = NULL; 
+
+//         int node_to_be_deleted = len - n;
+
+//         for(int i = 0;i<node_to_be_deleted;i++){
+//             prev = temp; 
+//             temp = temp->next;
+//         }
+
+//         // now delete
+//         prev->next = temp->next;
+
+//         // delete node
+//         delete (temp);
+//         return head;
+//     }
+// };
+
+// /*
+//     Analysis:
+//     Time Complexity : O(n)
+//     Space Complexity : O(1)
+// */    
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -8,6 +67,8 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+
 class Solution {
 public:
 
@@ -23,29 +84,29 @@ public:
 
 
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        int len = getLengthOfLinkedList(head);
+        // Move temp n node ahead
+        ListNode *temp = head;
+        ListNode *prev = head;
 
-        if(len == n){
-            ListNode *temp = head->next;
-            delete (head);
-            return temp;
-        }
-
-        ListNode * temp = head;
-        ListNode *prev = NULL; 
-
-        int node_to_be_deleted = len - n;
-
-        for(int i = 0;i<node_to_be_deleted;i++){
-            prev = temp; 
+        for(int i = 0;i<n;i++){
             temp = temp->next;
         }
 
-        // now delete
-        prev->next = temp->next;
+        if(temp == NULL){
+            ListNode *result = head->next;
+            delete (head);
+            return result;
+        }
 
-        // delete node
-        delete (temp);
+        while(temp!=NULL && temp->next!=NULL){
+            prev = prev->next;
+            temp = temp->next;
+        }
+
+        ListNode * delNode = prev->next;
+        prev->next = prev->next->next;
+        delete (delNode);
+
         return head;
     }
 };
