7adbaddc714f6737e9bcc2788247511b8feb4835
pravin817 <83346334+pravin817@users.noreply.github.com>
2024-03-06 23:17:12 +0530

    Time: 16 ms (12.05%) | Memory: 14.2 MB (5.66%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -8,19 +8,35 @@
  */
 class Solution {
 public:
+
+    // solution using the Hash Table
     bool hasCycle(ListNode *head) {
-        ListNode *slow = head;
-        ListNode* fast = head;
+        unordered_set<ListNode*>visited_nodes;
 
-        while(fast!=NULL && fast->next !=NULL){
-            slow = slow->next;
-            fast = fast->next->next;
+        ListNode* currentNode = head;
 
-            if(slow == fast){
+        // Traverse the linked List
+        while(currentNode != nullptr){
+
+            // check if the currentNode is found in the hash Table
+            if(visited_nodes.find(currentNode)!=visited_nodes.end()){
                 return true;
             }
+
+            // if the currentNode is not in the hash Table then insert it into the hash table
+            visited_nodes.insert(currentNode);
+
+            //move to the next node
+            currentNode = currentNode->next;
         }
 
-        return false;
+        return false; // if we get null
     }
 };
+
+
+/*
+    Analysis:
+    Time Complexity : O(n)
+    Space Complexity : O(n)
+*/    
