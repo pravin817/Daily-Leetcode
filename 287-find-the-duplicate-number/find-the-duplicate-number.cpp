8ecc34e7f0e05877b6f48bea73bb9db2e54b9dbd
pravin817 <83346334+pravin817@users.noreply.github.com>
2024-03-25 00:18:17 +0530

    Time: 317 ms (5.02%) | Memory: 108.2 MB (6.40%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -71,9 +71,36 @@ public:
     /*
         Analysis:
         Time Complexity : O(nlogn)
-        Space Complexity : O(1)
+        Space Complexity : O(n)
     */    
 
+    // Solution - 4 - By using the slow and fast pointer
+    int solution4(vector<int>nums){
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow!=fast);
+
+        // start the fast pointer from nums[0];
+
+        fast = nums[0];
+
+        while(slow!=fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+
+    /*
+        Analysis:
+        Time Complexity : O(n)
+        Space Complexity : O(1)
+    */    
 
     int findDuplicate(vector<int>& nums) {
         return solution3(nums);
