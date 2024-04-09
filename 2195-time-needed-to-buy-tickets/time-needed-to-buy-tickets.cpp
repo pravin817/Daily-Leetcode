a56a77d89a11ee37e25a8d592ffa2981d0601e1d
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2024-04-10 00:05:50 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (91.31%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
-    int timeRequiredToBuy(vector<int>& tickets, int k) {
+
+    // Brute Force by using the queue
+    int solution1(vector<int>&tickets, int k){
         int len = tickets.size();
         int time = 0;
         queue<int>q;
@@ -34,11 +36,69 @@ public:
 
         return time;
     }
+
+    /*
+        Analysis:
+        Time Complexity : O(n*m)
+        Sapce Complexity : O(n)
+    */    
+
+    // Brute Force by using the array
+    int solution2(vector<int>&tickets, int k){
+        int len = tickets.size();
+
+        int time = 0;
+
+        if(tickets[k] == 1){
+            return k+1;
+        }
+
+        while(tickets[k]>0){
+            for(int i = 0;i<len;i++){
+                if(tickets[i]!=0){
+                    tickets[i]--;
+                    time++;
+                }
+
+                if(tickets[k] == 0){
+                    return time;
+                }
+            }
+        }
+
+        return time;
+    }
+
+    /*
+        Analysis:
+        Time Complexity : O(n*m)
+        Sapce Complexity : O(1)
+    */
+
+    // Single pass solution
+    int solution3(vector<int>&tickets, int k){
+        int time = 0;
+        int len = tickets.size();
+
+        for(int i = 0;i<len;i++){
+            if(i<=k){
+                time+=min(tickets[i],tickets[k]);
+            }else{
+                time+=min(tickets[k]-1,tickets[i]);
+            }
+        }
+        return time;
+    } 
+
+    /*
+        Analysis:
+        Time Complexity : O(N)
+        Space Complexity : O(1)
+    */    
+
+
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        return solution3(tickets,k);
+    }
 };
 
-
-/*
-    Analysis:
-    Time Complexity : O(n*m)
-    Sapce Complexity : O(n)
-*/    
