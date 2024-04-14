cf9e4590ece9a2cb5b3bd18b7d2cfdd0153e5444
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2024-04-14 17:40:42 +0530

    Time: 3 ms (56.94%) | Memory: 14.9 MB (15.62%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.cpp
+++ 404-sum-of-left-leaves/sum-of-left-leaves.cpp
@@ -12,8 +12,8 @@
 class Solution {
 public:
 
+    // Solution by using the parent Node
     int solution1(TreeNode*curr, TreeNode *parent){
-
         if(curr == NULL){
             return 0;
         }
@@ -40,7 +40,32 @@ public:
         Space Complexity :O(1)  // Auxiiliary Space and O(depth of tree) recursion system stack space
     */    
 
+    //Solution by using the isLeft flag
+
+    int solution2(TreeNode *curr, bool isLeft){
+        // Base case
+        if(curr == NULL){
+            return 0;
+        }
+
+        // check if the node is left leaf node
+        if(curr->left == NULL && curr->right == NULL && isLeft  == true){
+            return curr->val;
+        }
+
+        // small calculation + Recursive call
+        return solution2(curr->left, true)+solution2(curr->right, false);
+    }
+
+
+    /*
+        Analysis:
+        Time Complexity : O(N)
+        Space Complexity : O(1) //Auxiiliary Space and O(depth of tree) recursion system stack space
+    */    
+
     int sumOfLeftLeaves(TreeNode* root) {
-        return solution1(root,NULL);
+        // return solution1(root,NULL);
+        return solution2(root, false);
     }
 };
