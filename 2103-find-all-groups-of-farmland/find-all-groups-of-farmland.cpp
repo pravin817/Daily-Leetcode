c75b1f2811b67c026bebd9b4b78aab64f8e455ba
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2024-04-20 23:19:12 +0530

    Time: 88 ms (92.73%) | Memory: 59.4 MB (84.36%) - LeetSync

--- 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.cpp
+++ 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.cpp
@@ -65,25 +65,74 @@ public:
         Space Complexity : O(n*m)
     */  
 
-    vector<vector<int>> findFarmland(vector<vector<int>>& land) {
-        m = land.size();
-        n = land[0].size();
+    vector<vector<int>>solution3(vector<vector<int>> &land){
+        int m = land.size();
+        int n = land[0].size();
+
+        vector<vector<int>>result;
 
         for(int i = 0;i<m;i++){
             for(int j = 0;j<n;j++){
-                if(land[i][j] == 1){
-                    int r1 = i;
-                    int c1 = j;
-                    int r2 = -1;
-                    int c2 = -1;
 
-                    BFS(land,i,j,r2,c2);
+                if(land[i][j] == 0){
+                    continue;
+                }
 
-                    ans.push_back({r1,c1,r2,c2});    
-                 }
+                // find the right most column of the current group
+                int c1 = j;
+                while(c1<n && land[i][c1] == 1){
+                    c1++;
+                }
+
+                // find the right most row of the current group
+                int r1 = i;
+                while(r1<m && land[r1][j] == 1){
+                    r1++;
+                }
+
+                c1 = c1 == 0 ? c1 : c1-1;
+                r1 = r1 == 0 ? r1 : r1-1;
+
+                result.push_back({i,j,r1,c1});
+                // mark as visited
+                for(int k = i;k<=r1;k++){
+                    for(int l = j;l<=c1;l++){
+                        land[k][l] = 0;
+                    }
+                }
             }
         }
 
-        return ans;
+        return result;
+    }
+
+    /*
+        Analysis:
+        Time Complexity : O(n*m)
+        Space Complexity : O(1)
+    */    
+
+    vector<vector<int>> findFarmland(vector<vector<int>>& land) {
+        // m = land.size();
+        // n = land[0].size();
+
+        // for(int i = 0;i<m;i++){
+        //     for(int j = 0;j<n;j++){
+        //         if(land[i][j] == 1){
+        //             int r1 = i;
+        //             int c1 = j;
+        //             int r2 = -1;
+        //             int c2 = -1;
+
+        //             BFS(land,i,j,r2,c2);
+
+        //             ans.push_back({r1,c1,r2,c2});    
+        //          }
+        //     }
+        // }
+
+        // return ans;
+
+        return solution3(land);
     }
 };
