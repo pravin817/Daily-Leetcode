d19d4483c04f40d7713f4881832dab66a906caf1
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2024-06-18 19:19:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (72.36%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -11,7 +11,46 @@ public:
         SC : O(logn)
     */    
 
+    // counting sort
+    void solution2(vector<int>&nums){
+        int countZero = 0, countOne = 0, countTwo = 0;
+
+        for(int i = 0;i<nums.size();i++){
+            if(nums[i] == 0){
+                countZero++;
+            }else if(nums[i] == 1){
+                countOne++;
+            }else{
+                countTwo++;
+            }
+        }
+
+
+        // Now override the actual array/ vector
+        int i = 0;
+        while(countZero){
+            nums[i++]=0;
+            countZero--;
+        }
+
+        while(countOne){
+            nums[i++]=1;
+            countOne--;
+        }
+
+        while(countTwo){
+            nums[i++] = 2;
+            countTwo--;
+        }
+    }
+
+    /*
+        Analysis:
+        TC : O(N)
+        SC : O(1)
+    */    
+
     void sortColors(vector<int>& nums) {
-        solution1(nums);
+        solution2(nums);
     }
 };
