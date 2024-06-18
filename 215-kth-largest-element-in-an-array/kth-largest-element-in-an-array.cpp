fba9765433c5e0174485b9b788505dfa5a44c171
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2024-06-18 18:11:41 +0530

    Time: 136 ms (8.52%) | Memory: 61.2 MB (31.91%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -13,7 +13,29 @@ public:
         Space Complexity : O(logn)
     */    
 
+    int solution2(vector<int>&nums, int k){
+        
+        priority_queue<int,vector<int>,greater<int>>pq;
+
+        for(int num:nums){
+            pq.push(num);
+
+            // check if the size of the minHeap become greater than k
+            if(pq.size()>k){
+                pq.pop();
+            }
+        }
+
+        return pq.top();
+    }
+
+    /*
+        Analysis:
+        Time Complexity : O(nlogk)
+        Space Complexity : O(k)
+    */    
+
     int findKthLargest(vector<int>& nums, int k) {
-        return solution1(nums,k);
+        return solution2(nums,k);
     }
 };
