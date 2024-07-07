36ff5c3b30c1dc922c1fadbb0f9b8b4c4790a009
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2024-07-07 09:57:37 +0530

    Time: 3 ms (96.28%) | Memory: 21.1 MB (65.01%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -26,8 +26,27 @@ public:
         Space Complexity : O(N) --> Because we have used the set
     */    
 
+    // Optimal Solution - By using the two pointer approach
+    int solution2(vector<int>&nums){
+        int n = nums.size();
+        int i = 0;
+        for(int j = 0;j<n;j++){
+            if(nums[i]!=nums[j]){
+                nums[i+1] = nums[j];
+                i++;
+            }
+        }
+
+        return i+1;
+    }
+
+    /*
+        Analysis:
+        Time COmplexity : O(N)
+        Space Complexity : O(1)
+    */    
 
     int removeDuplicates(vector<int>& nums) {
-        return solution1(nums);
+        return solution2(nums);
     }
 };
