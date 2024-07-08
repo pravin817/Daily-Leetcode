ba29d3dd13b28f543384f734214099af13a41c43
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2024-07-08 22:44:18 +0530

    Time: 3 ms (65.36%) | Memory: 7.2 MB (62.20%) - LeetSync

---
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+
+    int helper(int n, int k){
+        // base case 
+        
+        if(n == 1){
+            return 0;
+        }
+
+        return (helper(n-1,k)+k)%n;
+    }
+
+    int findTheWinner(int n, int k) {
+        return helper(n,k)+1;
+    }
+};
