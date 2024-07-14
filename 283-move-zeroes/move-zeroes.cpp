4369df557a0e791c71e558e49f9a59d461fbbd66
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2024-07-14 12:02:37 +0530

    Time: 17 ms (46.81%) | Memory: 22 MB (10.72%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -60,8 +60,38 @@ public:
         Time Complexity : O(N)+O(N-K) ==> O(N)
         Space Complexity : O(1)
     */    
+
+    void solution3(vector<int>&nums){
+        int n = nums.size();
+
+        int j = -1;
+
+        for(int i = 0;i<n;i++){
+            if(nums[i] == 0){
+                j = i;
+                break;
+            }
+        }
+
+        if(j == -1){
+            return;
+        }
+
+        for(int i = j+1;i<n;i++){
+            if(nums[i]!=0){
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
+    }
+
+    /*
+        Analysis:
+        Time Complexity : O(N)
+        Space Complexity : O(1)
+    */    
     
     void moveZeroes(vector<int>& nums) {
-        solution2(nums);
+        solution3(nums);
     }
 };
