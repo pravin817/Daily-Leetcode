897bb2c3abc3437637edf6ea6cb3724ff7a772ab
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2024-09-12 00:28:15 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (44.87%) - LeetSync

---
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int countNumberOfSetBits(int number)
+    {
+        int setBitsCount = 0;
+
+        while(number)
+        {
+            int lastBit = number % 2;
+
+            setBitsCount += (lastBit & 1);
+
+            // update the number
+            number = number / 2;
+        }
+        return setBitsCount;
+    }
+    int minBitFlips(int start, int goal) {
+        
+        int number = start ^ goal;
+        return countNumberOfSetBits(number);
+    }
+
+    /*
+        Analysis:
+        Time Complexity : O(logN)
+        Space Complexity : O(1)
+    */    
+};
