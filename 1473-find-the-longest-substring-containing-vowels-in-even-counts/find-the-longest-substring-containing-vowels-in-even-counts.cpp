a94f57a53e2d8356fe9c329af9839fd4c776d32f
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2024-09-16 01:11:19 +0530

    Time: 1012 ms (5.04%) | Memory: 23.3 MB (7.11%) - LeetSync

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -55,7 +55,69 @@ public:
         }
         return result;
     }
+    /*
+        Analysis:
+        Time Complexity : O(N)
+        Space Complexity : O(N)
+    */    
 
+
+    // Same as above just small improvement ( vowelCount[i] = (vowelCount[i]+1)%2)    => vowelCount[i] ^=1
+
+    int solution2(string s)
+    {
+        vector<int>vowelsCount(5,0);  // a,e,i,o,u
+        map<string,int>mp;            // String (state -> index)
+        string currentState = "";
+        int length = s.size();
+        int result = 0;
+
+        // Initial state
+        mp["00000"] = -1;
+
+        for(int i = 0; i < length ; ++i)
+        {
+            
+            if(s[i] == 'a')
+            {
+                 vowelsCount[0] ^= 1;             
+            }
+            else if(s[i] == 'e')
+            {
+                    vowelsCount[1] ^= 1;
+            }
+            else if(s[i] == 'i')
+            {
+                    vowelsCount[2] ^= 1;
+            }
+            else if(s[i] == 'o')
+            {
+                    vowelsCount[3] ^= 1;
+            }
+            else if(s[i] == 'u')
+            {
+                    vowelsCount[4] ^= 1;
+            }
+
+            // form the current state
+            currentState = "";
+            for(int j = 0;j <5;++j)
+            {
+                currentState += to_string(vowelsCount[j]);
+            }
+
+            // Now find in the map
+            if(mp.find(currentState)!=mp.end())
+            {
+                result = max(result, i - mp[currentState]);
+            }
+            else
+            {
+                mp[currentState] = i;
+            }
+        }
+        return result;
+    }
     /*
         Analysis:
         Time Complexity : O(N)
@@ -63,6 +125,6 @@ public:
     */    
 
     int findTheLongestSubstring(string s) {
-        return solution1(s);
+        return solution2(s);
     }
 };
