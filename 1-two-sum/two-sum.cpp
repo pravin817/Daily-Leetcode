f1f5cb24fef2fbc607f520244ac1acdebe73e3dd
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2024-09-17 23:11:10 +0530

    Time: 4 ms (93.77%) | Memory: 14.5 MB (18.41%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
 
+    // Check for the each elements in the array
     vector<int> solution1(vector<int>&nums, int target)
     {
         vector<int>result;
@@ -28,7 +29,44 @@ public:
         Space Complexity : O(1)
     */    
 
+    // As the each input has exactly one solution, we can store the elements in map along with their index and find in the map if found then we will return the answer.
+
+    vector<int> solution2(vector<int>&nums, int target)
+    {
+        int length = nums.size();
+        // value -> idx
+        std::map<int,int>mp;
+        vector<int>result;
+
+        // put the first element
+        mp[nums[0]] = 0;
+
+        // now traverse the all elements from the vector
+        for(int i = 1; i<length; ++i)
+        {
+            int valueToBeFind = target - nums[i];
+            if(mp.find(valueToBeFind)!=mp.end())
+            {
+                // If the pair is found then 
+                result.push_back(mp[valueToBeFind]);
+                result.push_back(i);
+                break;
+            }
+            else
+            {
+                mp[nums[i]] = i;
+            }
+        }
+        return result;
+    }
+
+    /*
+        Analysis:
+        Time Complexity : O(NlogN)
+        Space Complexity : O(N)
+    */    
+
     vector<int> twoSum(vector<int>& nums, int target) {
-        return solution1(nums,target);
+        return solution2(nums,target);
     }
 };
