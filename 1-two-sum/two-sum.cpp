d4ba1f9439a5f46fa6babcba20f7b6fcb771379f
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2024-12-28 23:55:32 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (23.06%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -23,7 +23,35 @@ public:
         Space Complexity : O(1)
     */
 
+    // Better Solution : we can store the elements in the map and find if the complement of the number is found means we got the pair, Here complement means target - nums[i] = element , where element we found in the map
+
+    std::vector<int> solution2(const std::vector<int> &nums, int target){
+        int n = nums.size();
+
+        unordered_map<int,int> mp;
+        // Element and its index
+
+        for(int i = 0; i < n; ++i){
+            int complement = target - nums[i];
+
+            // If the complement is found
+            if(mp.find(complement) != mp.end()){
+                return {mp[complement],i};
+            } else {
+                mp.insert({nums[i], i});
+            }
+        }
+
+        return {};
+    }
+
+    /*
+        Analysis:
+        Time Complexity : O(n)  , If the map is unordered if we used just map (then TC : O(nlogn))
+        Space Complexity : O(n)   , Where n is the number of the key-value pair in map
+    */
+
     vector<int> twoSum(vector<int>& nums, int target) {
-        return solution1(nums, target);
+        return solution2(nums, target);
     }
 };
