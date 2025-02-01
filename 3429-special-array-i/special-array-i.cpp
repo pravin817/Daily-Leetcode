e532124ce970d6feafdb257ef7b8b5ec66943c35
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2025-02-02 00:25:45 +0530

    Time: 0 ms (100.00%) | Memory: 27.8 MB (25.00%) - LeetSync

--- 3429-special-array-i/special-array-i.cpp
+++ 3429-special-array-i/special-array-i.cpp
@@ -1,7 +1,10 @@
 class Solution {
 public:
 
-    // Solution : 1 -> Brute Force Solution
+    // Solution : 1 -> Brute Force Solution using the sum of the two numbers
+    // Odd Number  (1) + Odd Number (3)  = Even Number (4)
+    // Even Number (2) + Even Number(4)  = Even Number (6)
+    // Odd Number  (1) + Even Number(2)  = Odd Number  (3)
     bool solution1(const std::vector<int>&nums) {
         for(int i = 1; i < nums.size();++i) {
             int sum = nums[i] + nums[i-1];
@@ -14,7 +17,28 @@ public:
 
     /*
         Analysis:
-        Time Complexity : O(N)
+        Time Complexity  : O(N)
+        Space Complexity : O(1)
+    */
+
+    /*
+        Solution 2 By using the modulo operator (%)
+        When number % 2 == 0, Means the number is even number
+             number % 2 == 1, Means the number is odd number
+    */
+
+    bool solution2(const std::vector<int>&nums) {
+        for(int index = 0; index < nums.size() - 1; ++index) {
+            if(nums[index] % 2 == nums[index+1] % 2) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /*
+        Analysis:
+        Time Complexity  : O(N)
         Space Complexity : O(1)
     */
 
