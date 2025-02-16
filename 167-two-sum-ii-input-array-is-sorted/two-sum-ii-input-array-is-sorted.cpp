f0be8310e01eb10bb8e7dd507903bd8664d438f3
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2025-02-16 23:48:48 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (32.52%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,98 +1,50 @@
 class Solution {
 public:
 
+    // Brute Force Solution by using the nested for loops
+    std::vector<int> solution1(const std::vector<int>&nums, int target) {
+        int n = nums.size();
 
-    // Brute Force solution : Check for the each pair in the vector
-    // GIVES TLE
-    vector<int> solution1(vector<int>&nums, int target)
-    {
-        int length = nums.size();
-
-        for(int i = 0; i < length ; ++i)
-        {
-            for(int j = i+1; j < length; ++j)
-            {
-                if(nums[i] + nums[j] == target)
-                {
-                    return {i+1, j+1};
+        for(int i = 0; i < n; ++i) {
+            for(int j = i+1; j < n; ++j) {
+                if(nums[i] + nums[j] == target) {
+                    return {i+1, j+1};  // As the index are 1 based
                 }
             }
         }
+
         return {};
     }
 
     /*
         Analysis:
-        Time Complexity : O(N^2)
+        Time Complexity  : O(N^2)
         Space Complexity : O(1)
-    */    
+    */
 
-    //Solution 2 - By using the map for the storing the element along with the index and search for the (target- nums[i]), if the remaining found in the map then returns the indices.
+    // Solution : 2 We can optimised the above solution (The above solution gives the TLE) by using the map who can keep the track of the element that we need for the 2 sum
+    std::vector<int> solution2(const std::vector<int>&nums, int target) {
+        int n = nums.size();
+        std::unordered_map<int,int> indexMap;
 
-    vector<int>solution2(vector<int>&nums, int target)
-    {
-        int length = nums.size();
-
-        unordered_map<int,int>mp;
-
-        // Traverse the complete array and simulatenously check if the remaining is present (find) in the map then return the indices of the two elements
-
-        for(int i = 0 ; i < length ; ++i)
-        {
+        for(int i = 0; i < n; ++i){
             int remaining = target - nums[i];
-
-            if(mp.find(remaining)!=mp.end())
-            {
-                // Found
-                return {mp[remaining]+1, i+1};  // Indices starts from 1
+            // Check if the remaining is present in the map
+            if(indexMap.find(remaining) != indexMap.end()) {
+                return {indexMap[remaining] + 1, i+1}; // As the index is 1 based
             }
-            else
-            {
-                // Insert the elements into the map
-                mp[nums[i]] = i;
-            }
+            indexMap[nums[i]] = i;
         }
-        return {};  // If the pair not found
-    }
-    /*
-        Analysis:
-        Time Complexity : O(N)
-        Space Complexity : O(N)
-    */    
-
-    // Expected Solution : As the array is sorted in increasing order we can think of two pointer approach.
-    vector<int> solution3(vector<int>&nums, int target)
-    {
-        int low = 0;
-        int high = nums.size()-1;
-
-        while(low<high)
-        {
-            int sum = nums[low] + nums[high];
-
-            if(sum == target)
-            {
-                return {low+1, high+1};
-            }
-            else if(sum < target)
-            {
-                ++low;
-            }
-            else
-            {
-                --high;
-            }
-        }
-
         return {};
     }
+
     /*
         Analysis:
-        Time Complexity : O(N)
-        Space Complexity : O(1)
-    */    
+        Time Complexity  : O(N)
+        Space Complexity : O(N)
+    */
 
     vector<int> twoSum(vector<int>& numbers, int target) {
-        return solution3(numbers,target);
+        return solution2(numbers, target);
     }
 };
