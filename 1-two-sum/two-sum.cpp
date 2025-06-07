01b8bdb7e3efba560a44a93db8842825c3141fca
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2025-06-07 23:15:41 +0530

    Time: 46 ms (20.69%) | Memory: 14.1 MB (63.98%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,51 +1,25 @@
 class Solution {
 public:
 
-    // Solution - 1 : Brute Force solution 
-    std::vector<int> findTwoSum(const std::vector<int>&nums, int target) {
-        int n = nums.size();
-
-        for(int i = 0; i < n; ++i) {
-            for(int j = i+1; j < n; ++j) {
+    // Brute Force solution : By iterating over the elements from the vector
+    std::vector<int> bruteForceSolution(const std::vector<int>&nums, int target) {
+        for(int i = 0; i < nums.size(); ++i) {  // O(n)
+            for(int j = i+1; j < nums.size(); ++j) { // O(n)
                 if(nums[i] + nums[j] == target) {
                     return {i,j};
                 }
             }
         }
-
-        return {};
+        return {-1,-1};
     }
 
     /*
         Analysis:
-        Time Complexity  : O(N^2)
-        Space Complexity : O(1)
+            Time Complexity : O(n^2)
+            Space Complexity : O(1)
     */
-
-    // We can optimised the above solution by using the unordered_map
-    std::vector<int> findTwoSum2(const std::vector<int>&nums, int target) {
-        std::unordered_map<int,int>indexMap;
-
-        for(int i = 0; i < nums.size(); ++i) {
-            int complement = target - nums[i];
-
-            if(indexMap.find(complement)!= indexMap.end()){
-                return {indexMap[complement], i};
-            } 
-
-            indexMap[nums[i]] = i;
-        }
-
-        return {};
-    }
-
-    /*
-        Analysis:
-        Time Complexity : O(N)
-        Space Complexity : O(N)
-    */
-
+    
     vector<int> twoSum(vector<int>& nums, int target) {
-        return findTwoSum2(nums, target);
+        return bruteForceSolution(nums, target);
     }
 };
