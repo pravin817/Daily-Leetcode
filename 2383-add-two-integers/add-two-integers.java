158b70de915dacba290e7c3b083911f03ff1e963
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2025-06-29 10:23:41 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (63.97%) - LeetSync

---
+++ 2383-add-two-integers/add-two-integers.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    /*
+        Function    : sum
+        Description : The function is used for the addition of the two numbers.
+        @author     : Pravin Mhaske
+        @param      : int num1, int num2
+        @return     : int - sum of two numbers
+        Date        : 29/06/2025
+    */
+    private int sumOfTwoNumbers(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    /*
+        Analysis:
+        Time Complexity  : O(1)
+        Space Complexity : O(1)
+    */
+    
+    public int sum(int num1, int num2) {
+        return sumOfTwoNumbers(num1, num2);
+    }
+}
