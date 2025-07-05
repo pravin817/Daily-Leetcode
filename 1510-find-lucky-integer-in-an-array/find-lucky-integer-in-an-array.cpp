453556bee2dc79540192ff7df479eed1cedf4127
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2025-07-05 23:38:11 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (5.42%) - LeetSync

--- 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.cpp
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.cpp
@@ -27,7 +27,30 @@ public:
         Space Complexity : O(n)
     */
 
+    // We can use the array / hash map as the size of the input is 500 
+    int findLuckyNumber2(const std::vector<int>&nums) {
+        int store[501] = {};
+
+        for(auto n: nums) {
+            ++store[n];
+        }
+
+        for(auto n = nums.size(); n > 0; --n) {
+            if (n == store[n]) {
+                return n;
+            }
+        }
+
+        return -1;
+    }
+
+    /*
+        Analysis:
+        Time Complexity  : O(n)
+        Space Complexity : O(n)
+    */
+
     int findLucky(vector<int>& arr) {
-        return findLuckyNumber(arr);
+        return findLuckyNumber2(arr);
     }
 };
