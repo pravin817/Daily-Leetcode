ccd1396238d36617952a4cb760c8b6d614315ce2
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2025-08-01 22:52:05 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (30.63%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>>ans;
+        vector<vector<int>>ans(numRows);
 
         for(int i = 0;i<numRows;i++){
-            vector<int>v(i+1,1);
+            ans[i] = vector<int>(i+1,1);
 
-            // Update the ans
-            for(int j = 1 ;j<i;j++){
-                v[j] = ans[i-1][j] + ans[i-1][j-1];
+            for(int j = 1;j<i;j++){
+                ans[i][j] = ans[i-1][j] + ans[i-1][j-1];
             }
-            ans.push_back(v);
         }
-
         return ans;
     }
 };
+
+/*
+    This is the DP Approach
+    Analysis:
+    Time Complexity : O(n^2)
+    Space Complexity : O(1)
+*/    
