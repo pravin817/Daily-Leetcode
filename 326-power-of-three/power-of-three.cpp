c6f67ec8e9f727fc0f954e587ab74449b045b89d
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2025-08-18 22:40:42 +0530

    Time: 4 ms (48.10%) | Memory: 8.9 MB (47.92%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,12 +1,41 @@
 class Solution {
 public:
-    bool isPowerOfThree(int n) {
-        return (n > 0 && 1162261467 % n == 0);
+
+    // Function used to check if the number is power of 3
+    bool solution1(int n) {
+
+        // Base case
+        if (n <= 0) {
+            return false;
+        }
+
+        while(n > 0) {
+            int remainder = n % 3;
+
+            if (remainder == 0) {
+                n = n / 3;
+            } else {
+                if (n == 1){
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+        }
+
+        return false;
     }
 
     /*
         Analysis:
-        Time Complexity  : O(1)
+        Time Complexity  : O(logn)  ---> log n base 3
         Space Complexity : O(1)
     */
+
+
+
+    bool isPowerOfThree(int n) {
+        return solution1(n);
+    }
 };
