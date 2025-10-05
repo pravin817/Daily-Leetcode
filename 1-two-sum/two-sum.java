9301960bcf6271a3b8c98c989a8659932ab1fc9c
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2025-10-05 22:50:49 +0530

    Time: 38 ms (43.72%) | Memory: 45.1 MB (32.02%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -22,7 +22,29 @@ class Solution {
         */
     }
 
+    // Better solution - As we don't take the same element twice so instead of starting from 0 each time we can start from i+1
+    private int[] betterSolution(int[] nums, int target) {
+        int length = nums.length;
+
+        for (int i = 0; i < length; ++i) {
+            for (int j = i+1; j < length; ++j) {
+                if(nums[i] + nums[j] == target) {
+                    return new int[] {i,j};
+                }
+            }
+        }
+
+        // return the -1, -1 if there is no elements whose sum is equal to the target
+        return new int[] {-1, -1};
+    }
+
+    /*
+        Analysis:
+            Time Complexity  : O(n^2)
+            Space Complexity : O(1)
+    */
+
     public int[] twoSum(int[] nums, int target) {
-        return bruteForceSolution(nums, target);
+        return betterSolution(nums, target);
     }
 }
