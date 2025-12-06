292bc65ea01a70da9c90af9ae4de7e013fad50d1
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2025-12-07 00:30:10 +0530

    Time: 3 ms (32.92%) | Memory: 43.1 MB (52.29%) - LeetSync

--- 4100-complete-prime-number/complete-prime-number.java
+++ 4100-complete-prime-number/complete-prime-number.java
@@ -6,7 +6,7 @@ class Solution {
             return false;
         }
 
-        for (int i = 2; i < num; ++i) {
+        for (int i = 2; i*i <= num; ++i) {
             if (num % i == 0) {
                 return false;
             }
