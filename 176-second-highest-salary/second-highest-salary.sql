18c79f38b20b0d24cc0e56bf18b03f4cd07aaec5
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2025-12-17 00:11:11 +0530

    Time: 268 ms (43.57%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,6 +1,14 @@
+-- SELECT MAX(salary) as SecondHighestSalary
+-- FROM employee 
+-- WHERE salary < (
+--     SELECT max(salary) 
+--     FROM employee
+-- );
+
 SELECT MAX(salary) as SecondHighestSalary
-FROM employee 
-WHERE salary < (
-    SELECT max(salary) 
+FROM (
+    SELECT DISTINCT Salary 
     FROM employee
-);
+    ORDER BY Salary DESC
+    LIMIT 1 OFFSET 1
+) t;
