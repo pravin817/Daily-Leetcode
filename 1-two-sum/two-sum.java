d24d534007ef7ef313efdbfee264b10d65829261
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2026-01-01 00:07:07 +0530

    Time: 2 ms (99.12%) | Memory: 47.4 MB (9.15%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,39 +1,33 @@
 class Solution {
-    // Function used to find the indices of the elements whose sum equal to target
+
+    // Brute Force solution check for the each possible pair
     private int[] bruteForceSolution(int[] nums, int target) {
-        int length = nums.length;
-
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < length; j++) {
-                if (nums[i] + nums[j] == target && i != j) {
-                    return new int[] {i,j};
+        for (int i = 0; i < nums.length; ++i) {
+            for (int j = 0; j < nums.length; ++j) {
+                if(nums[i] + nums[j] == target && i != j) {
+                    return new int[]{i, j};
                 }
             }
         }
 
-        return new int[] {-1,-1};
-
-        /*
-            Analysis:
-                Time Complexity  : O(n^2)
-                Space Complexity : O(1)
-        */
+        return new int[]{-1, -1};
     }
+    /*
+        Analysis:
+            Time Complexity  : O(n^2)
+            Space Complexity : O(1)
+    */
 
-    // Better solution - As we don't take the same element twice so instead of starting from 0 each time we can start from i+1
-    private int[] betterSolution(int[] nums, int target) {
-        int length = nums.length;
-
-        for (int i = 0; i < length; ++i) {
-            for (int j = i+1; j < length; ++j) {
+    // Little bit optimisation
+    private int[] solutionTwo(int[] nums, int target) {
+        for(int i = 0; i < nums.length; ++i) {
+            for (int j = i+1; j < nums.length; ++j) {
                 if(nums[i] + nums[j] == target) {
-                    return new int[] {i,j};
+                    return new int[]{i, j};
                 }
             }
         }
-
-        // return the -1, -1 if there is no elements whose sum is equal to the target
-        return new int[] {-1, -1};
+        return new int[]{-1,-1};
     }
 
     /*
@@ -42,60 +36,29 @@ class Solution {
             Space Complexity : O(1)
     */
 
-    // We can optimised this solution by using the hashMap
-    private int[] optimalSolution(int[] nums, int target) {
-        int length = nums.length;
-
-        Map<Integer,Integer> map = new HashMap<Integer, Integer>();
-
-        // Add the element and its index to hashMap
-        for(int i = 0; i < length; ++i) {
-            map.put(nums[i], i);
-        }
-
-        // Traverse the array and check if we get the elements whose sum equals to target
-        for (int i = 0; i < length; ++i) {
-            int complement = target - nums[i];
-
-            if(map.containsKey(complement) && map.get(complement) != i ) {
-                return new int[] {map.get(complement), i};
-            }
-        }
-
-        return new int[]{};
-    }
-    /*
-        Analysis:
-            Time Complexity  : O(n)
-            Space Complexity : O(n)
-    */
-
-    // Instead of the two pass we can solve this in a single pass
-    private int[] optimalSolution2(int[] nums, int target) {
-        Map<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+    // Optimised solution - Store the element in hashMap and check if the element (complement = target - currentElement) exist in the hashMap then return the index of the current element along with the index of complement
+    private int[] optimisedSolution(int[] nums, int target) {
+        Map<Integer, Integer> elementTracker = new HashMap<Integer, Integer>();
 
         for (int i = 0; i < nums.length; ++i) {
             int complement = target - nums[i];
 
-            // Check if the complement is present in hashMap
-            if(hashMap.containsKey(complement)) {
-                return new int[] {i, hashMap.get(complement)};
+            if (elementTracker.containsKey(complement)) {
+                return new int[] {i, elementTracker.get(complement)};
             }
-
-            // Add the entry to the map
-            hashMap.put(nums[i],i);
+            elementTracker.put(nums[i], i);
         }
 
-        return new int[] {};
+        return new int[]{-1,-1};
     }
 
-    /*
+    /*  
         Analysis:
             Time Complexity  : O(n)
             Space Complexity : O(n)
     */
 
     public int[] twoSum(int[] nums, int target) {
-        return optimalSolution2(nums, target);
+        return optimisedSolution(nums, target);
     }
 }
