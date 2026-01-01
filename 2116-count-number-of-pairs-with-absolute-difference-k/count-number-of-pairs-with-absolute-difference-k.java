9c75091a8281920333f8fd3c2ccef2344140395e
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2026-01-01 07:29:50 +0530

    Time: 5 ms (76.34%) | Memory: 46.7 MB (5.84%) - LeetSync

--- 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.java
+++ 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.java
@@ -21,13 +21,33 @@ class Solution {
             Space Complexity : O(1)
     */
 
-    // We can optimised the above solution by using the hashmap
+    /*
+        We have given 
+            | a - b | = K
+
+            i.e 
+
+            a - b = K   or a - b = -K
+
+            Now find the value of a
+
+            a = b + K   or a = b - K
+
+            Here we want to find a based on the value of b and k as we have already given the k and our b is value from the array.
+
+            hence b = nums[i]
+
+            as we want the count we will store the frequency of the each element and check if we have the a then we add its frequency to the count.
+
+            and update the frequecy of the current element.
+    */
+
     private int countKDifferenceOptimal(int[] nums, int k) {
         int counter = 0;
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
 
         for (int num: nums) {
-            counter += map.getOrDefault(-k+num, 0) + map.getOrDefault(k+num, 0);
+            counter += map.getOrDefault(num - k, 0) + map.getOrDefault(num + k, 0);
             map.put(num, map.getOrDefault(num, 0)+1);
         }
 
