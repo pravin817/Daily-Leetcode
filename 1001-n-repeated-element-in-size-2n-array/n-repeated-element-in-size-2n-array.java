46f8891eb3229802a52f575253dc8cf5fbbc49a8
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2026-01-03 00:00:26 +0530

    Time: 17 ms (29.06%) | Memory: 47.3 MB (75.13%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
@@ -6,15 +6,19 @@ class Solution {
         int n = length / 2;
         Map<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
 
+        // Add the all elements to the hashMap
         for (int num: nums) {
-            if(hashMap.containsKey(num)) {
-                return num;
-            }
-
             hashMap.put(num, hashMap.getOrDefault(num,0) + 1);
         }
 
-        return -1;
+        // return the key whose value is greater than 1
+        for (int key: hashMap.keySet()) {
+            if (hashMap.get(key) > 1) {
+                return key;
+            }
+        }
+
+        return -1; 
     }
 
     /*
