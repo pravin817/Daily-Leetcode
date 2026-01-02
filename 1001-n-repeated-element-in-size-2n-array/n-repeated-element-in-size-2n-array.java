3d1cf98d09ba3a1e9242c886516fff52e82b0370
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2026-01-03 00:05:02 +0530

    Time: 0 ms (100.00%) | Memory: 47.6 MB (36.21%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
@@ -3,7 +3,6 @@ class Solution {
     // Solution
     private int getRepeatedNTimesElement(int[] nums) {
         int length = nums.length;
-        int n = length / 2;
         Map<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
 
         // Add the all elements to the hashMap
@@ -27,7 +26,30 @@ class Solution {
             Space Complexity : O(n)
     */
 
+    // little bit optimsed version of the above solution
+    private int getRepeatedElement(int[] nums) {
+        Map<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+
+        for (int num : nums) {
+            if (hashMap.containsKey(num)) {
+                return num;
+            }
+
+            // Add the element to the hashMap
+            hashMap.put(num, hashMap.getOrDefault(num, 0) + 1);
+        }
+
+        // It is always we get the solution but it is good practice to return -1 if there is no any solution as the elements from the nums are range from 0 <= nums[i] <= 10^4
+        return -1;
+    }
+
+    /*
+        Analysis:
+            Time Complexity  : O(n)
+            Space Complexity : O(n)
+    */
+
     public int repeatedNTimes(int[] nums) {
-        return getRepeatedNTimesElement(nums);
+        return getRepeatedElement(nums);
     }
 }
