75c527b78c7f798fd7af3d3015f16fcd9a3fae05
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2026-02-15 23:03:07 +0530

    Time: 2 ms (90.05%) | Memory: 47.8 MB (56.11%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -27,17 +27,54 @@ class Solution {
         }
     }
 
-    public void moveZeroes(int[] nums) {
-        int [] result = solution(nums);
+    // public void moveZeroes(int[] nums) {
+    //     int [] result = solution(nums);
 
-        for (int i = 0; i < nums.length; ++i) {
-            nums[i] = result[i];
+    //     for (int i = 0; i < nums.length; ++i) {
+    //         nums[i] = result[i];
+    //     }
+    // }
+
+
+    /*
+        Analysis:
+            Time Complexity  : O(n)
+            Space Complexity : O(n)
+    */
+
+    // Function used to swap the elements of the array
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    /*
+        Analysis:
+            Time Complexity  : O(1)
+            Space Complexity : O(1)
+    */
+
+    // Optimised solution
+    public void moveZeroes(int[] nums) {
+        int length = nums.length;
+        int start = 0, current = 0;
+
+        while (current < length) {
+            if (nums[current] == 0) {
+                current++;
+                continue;
+            }
+
+            swap(nums, current, start);
+            start++;
+            current++;
         }
     }
 
     /*
         Analysis:
             Time Complexity  : O(n)
-            Space Complexity : O(n)
+            Space Complexity : O(1)
     */
 }
