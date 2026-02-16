a8d55d23b971584a1bb828695c6e5f599b2dd409
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2026-02-16 23:10:38 +0530

    Time: 12 ms (34.02%) | Memory: 52.5 MB (90.70%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -12,8 +12,34 @@ class Solution {
             Space Complexity : O(1)
     */
 
+    // Better solution by using the hashMap
+    private int betterSolution(int[] nums) {
+        int n = nums.length;
+        Map<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+
+        for (int num: nums) {
+            hashMap.put(num, hashMap.getOrDefault(num, 0)+1);
+        }
+
+        // Return the elements whose frequency count is n / 2
+        int freqCount = n / 2;
+
+        for (Map.Entry<Integer, Integer> pair: hashMap.entrySet()) {
+            if (pair.getValue() > freqCount) {
+                return pair.getKey();
+            }
+        }
+
+        return -1;
+    }
+
+    /*
+        Analysis:
+            Time Complexity  : O(n)
+            Space Complexity : O(n)
+    */
 
     public int majorityElement(int[] nums) {
-        return bruteForceSolution(nums);
+        return betterSolution(nums);
     }
 }
