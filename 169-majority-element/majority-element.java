0b5b429809e5b8d07a482e52f0c89b1fc38755e4
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2026-02-17 23:04:14 +0530

    Time: 2 ms (71.04%) | Memory: 55.9 MB (31.34%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -39,7 +39,51 @@ class Solution {
             Space Complexity : O(n)
     */
 
+    // Optimal solution using the Boyer moore algorithms
+    private int optimalSolution(int[] nums) {
+
+        // Make the assumption that the first element is the majority element.
+        int candidate = nums[0];
+        int count = 1;
+
+        for (int i = 1; i < nums.length; ++i) {
+            if (nums[i] == candidate) {
+                ++count;
+            } else if (count == 0) {
+                candidate = nums[i];
+                count = 1;
+            } else {
+                --count;
+            }
+        }
+
+        // Now validate the selected element is the majority elements
+        // In a case of the n / 2 we have only one majority element
+        // for n / 3 = 2 (best case)
+        // for n / 4 = 3 (best case)
+        // for n / k = (k-1) (best case)
+
+        count = 0;
+        for (int num: nums) {
+            if (num == candidate) {
+                ++count;
+            }
+
+            if (count > nums.length / 2) {
+                return candidate;
+            }
+        }
+
+        return -1; // As in the question given there is always the majority element is present
+    }
+
+    /*
+        Analysis:
+            Time Complexity  : O(n)
+            Space complexity : O(1)
+    */
+
     public int majorityElement(int[] nums) {
-        return betterSolution(nums);
+        return optimalSolution(nums);
     }
 }
