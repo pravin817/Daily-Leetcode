54a8fa29077c8bd700e858110d883439a375ccd1
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2026-03-24 08:01:00 +0530

    Time: 28 ms (5.22%) | Memory: 83.7 MB (71.61%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,6 +1,24 @@
 class Solution {
 
-    private boolean bruteForceSolution(int[] nums) {
+    private boolean solution1(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+    /*
+        Analysis:
+            Time Complexity  : O(n^2)
+            Space Complexity : O(1)
+    */
+
+
+    private boolean solution2(int[] nums) {
         Arrays.sort(nums);
 
         // Check if the adjacent elements are same
@@ -19,7 +37,7 @@ class Solution {
     */
 
 
-    private boolean betterSolution(int[] nums) {
+    private boolean solution3(int[] nums) {
         Map<Integer, Integer> frequencyCount = new HashMap<Integer, Integer>();
 
         for (int num: nums) {
@@ -39,8 +57,38 @@ class Solution {
     */
 
 
-    public boolean containsDuplicate(int[] nums) {
-        return bruteForceSolution(nums);
+    private boolean solution4(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+
+            set.add(num);
+        }
+
+        return false;
     }
 
+    /*
+        Analysis:
+            Time Complexity  : O(n)
+            Space Complexity : O(n)
+    */
+
+    private boolean solution5(int[] nums) {
+        return Arrays.stream(nums).distinct().count() < nums.length;
+    }
+
+    /*
+        Analysis:
+            Time Complexity  : O(n)
+            Space Complexity : O(n)
+    */
+
+
+    public boolean containsDuplicate(int[] nums) {
+        return solution5(nums);
+    }
 }
