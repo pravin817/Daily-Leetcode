d935721f70d2a194ebaa93151f0a3a3357d6c78d
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2026-03-24 07:31:42 +0530

    Time: 24 ms (26.37%) | Memory: 76.5 MB (98.30%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,6 +1,25 @@
 class Solution {
 
     private boolean bruteForceSolution(int[] nums) {
+        Arrays.sort(nums);
+
+        // Check if the adjacent elements are same
+        for (int idx = 1; idx < nums.length; idx++) {
+            if (nums[idx] == nums[idx-1]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    /*
+        Analysis:
+            Time Complexity  : O(nlogn)
+            Space Complexity : O(1)
+    */
+
+
+    private boolean betterSolution(int[] nums) {
         Map<Integer, Integer> frequencyCount = new HashMap<Integer, Integer>();
 
         for (int num: nums) {
