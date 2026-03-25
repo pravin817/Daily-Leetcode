0217f9ab0e043bd5e4ab7c31f151e072e09f0b41
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2026-03-25 23:13:36 +0530

    Time: 17 ms (16.28%) | Memory: 46.7 MB (12.67%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -35,8 +35,42 @@ class Solution {
             Space Complexity : O(1)
     */
 
+    // Solution using the hashMap
+    private boolean solution2(String s, String t) {
+        if (s == null || t == null) {
+            return false;
+        }
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> freqTracker = new HashMap<Character, Integer>();
+
+        for (int idx = 0; idx < s.length(); ++idx) {
+            freqTracker.put(s.charAt(idx), freqTracker.getOrDefault(s.charAt(idx), 0) + 1);
+        }
+
+        for (int idx = 0; idx < t.length(); ++idx) {
+            freqTracker.put(t.charAt(idx), freqTracker.getOrDefault(t.charAt(idx), 0) - 1);
+        }
+
+        for (Map.Entry<Character, Integer> entry : freqTracker.entrySet()) {
+            if (entry.getValue() != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /*
+        Analysis:
+            Time Complexity  : O(n)
+            Space COmplexity : O(1)
+    */
 
     public boolean isAnagram(String s, String t) {
-        return solution1(s,t);
+        return solution2(s,t);
     }
 }
