ff507cd84360c62ba85cb64f8160367a1ab1245c
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2026-03-30 00:56:01 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (43.04%) - LeetSync

---
+++ 4264-first-matching-character-from-both-ends/first-matching-character-from-both-ends.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int firstMatchingIndex(String s) {
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            if(s.charAt(i) == s.charAt(n - i - 1)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    /*
+        Analysis:
+            Time Complexity  : O(n)
+            Space Complexity : O(1)
+    */
+}
