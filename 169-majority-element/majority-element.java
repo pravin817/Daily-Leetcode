541b3c9d033df4d1540a9490494b86ca8e0373a6
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2026-04-01 23:57:00 +0530

    Time: 1919 ms (5.08%) | Memory: 55.8 MB (26.05%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,32 +1,19 @@
 class Solution {
 
-    //Brute Force solution
-    private int bruteForceSolution(int[] nums) {
-        int length = nums.length;
-        Arrays.sort(nums);
-        return nums[length/2];
-    }
-    /*
-        Analysis:
-            Time Complexity  : O(nlogn)
-            Space Complexity : O(1)
-    */
-
-    // Better solution by using the hashMap
-    private int betterSolution(int[] nums) {
+    // Brute Force Solution
+    private int solution1(int[] nums) {
         int n = nums.length;
-        Map<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
 
-        for (int num: nums) {
-            hashMap.put(num, hashMap.getOrDefault(num, 0)+1);
-        }
+        for (int i = 0; i < n; ++i) {
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
 
-        // Return the elements whose frequency count is n / 2
-        int freqCount = n / 2;
-
-        for (Map.Entry<Integer, Integer> pair: hashMap.entrySet()) {
-            if (pair.getValue() > freqCount) {
-                return pair.getKey();
+            if (count > n / 2) {
+                return nums[i];
             }
         }
 
@@ -35,55 +22,11 @@ class Solution {
 
     /*
         Analysis:
-            Time Complexity  : O(n)
-            Space Complexity : O(n)
-    */
-
-    // Optimal solution using the Boyer moore algorithms
-    private int optimalSolution(int[] nums) {
-
-        // Make the assumption that the first element is the majority element.
-        int candidate = nums[0];
-        int count = 1;
-
-        for (int i = 1; i < nums.length; ++i) {
-            if (nums[i] == candidate) {
-                ++count;
-            } else if (count == 0) {
-                candidate = nums[i];
-                count = 1;
-            } else {
-                --count;
-            }
-        }
-
-        // Now validate the selected element is the majority elements
-        // In a case of the n / 2 we have only one majority element
-        // for n / 3 = 2 (best case)
-        // for n / 4 = 3 (best case)
-        // for n / k = (k-1) (best case)
-
-        count = 0;
-        for (int num: nums) {
-            if (num == candidate) {
-                ++count;
-            }
-
-            if (count > nums.length / 2) {
-                return candidate;
-            }
-        }
-
-        return -1; // As in the question given there is always the majority element is present
-    }
-
-    /*
-        Analysis:
-            Time Complexity  : O(n)
-            Space complexity : O(1)
+            Time Complexity  : O(n^2)
+            Space Complexity : O(1)
     */
 
     public int majorityElement(int[] nums) {
-        return optimalSolution(nums);
+        return solution1(nums);
     }
 }
