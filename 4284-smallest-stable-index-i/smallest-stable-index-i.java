7b8b2989d3c9f20c3f97230cc12a0e676f85245f
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2026-04-20 23:45:50 +0530

    Time: 1 ms (100.00%) | Memory: 46.4 MB (0.00%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.java
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.java
@@ -47,8 +47,45 @@ class Solution {
             Time Complexity  : O(n^2)
             Space Complexity : O(1)
     */
+
+    private int solution2(int[] nums, int k) {
+        int n = nums.length;
+
+        int[] maxiElement = new int[n];
+        int[] miniElement = new int[n];
+
+        // pre-compute the maxiElement
+        int maxi = nums[0];
+        for (int idx = 0; idx < n; idx++) {
+            maxi = Math.max(maxi, nums[idx]);
+            maxiElement[idx] = maxi;
+        }
+
+        // pre-compute the miniElement
+        int mini = nums[n-1];
+        for (int idx = n-1; idx >= 0; idx--) {
+            mini = Math.min(mini, nums[idx]);
+            miniElement[idx] = mini;
+        }
+
+        // Calculate the instability score
+        for (int idx = 0; idx < n; idx++) {
+            int instabilityScore = maxiElement[idx] - miniElement[idx];
+
+            if (instabilityScore <= k) {
+                return idx;
+            }
+        }
+
+        return -1;
+    }
+    /*
+        Analysis:
+            Time Complexity  : O(n)
+            Space Complexity : O(n)
+    */
     
     public int firstStableIndex(int[] nums, int k) {
-        return solution1(nums, k);
+        return solution2(nums, k);
     }
 }
