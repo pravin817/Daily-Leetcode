27b10c2bbe66bf82596a1fabdba590d38cf4228f
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2026-05-25 22:58:42 +0530

    Time: 1 ms (99.48%) | Memory: 47.3 MB (13.83%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.java
+++ 2048-build-array-from-permutation/build-array-from-permutation.java
@@ -1,5 +1,6 @@
 class Solution {
-    public int[] buildArray(int[] nums) {
+
+    private int[] bruteForceSolution(int[] nums) {
         int[] result = new int[nums.length];
 
         for (int idx = 0; idx < nums.length; idx++) {
@@ -8,10 +9,14 @@ class Solution {
 
         return result;
     }
-
     /*
         Analysis:
             Time Complexity :  O(n)
             Space Complexity : O(1)
     */
+
+    public int[] buildArray(int[] nums) {
+        return bruteForceSolution(nums);
+    }
+
 }
