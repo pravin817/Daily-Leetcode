fc5206a3cbeabc3fdead381b149a20df3c63c79e
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2026-05-28 23:20:36 +0530

    Time: 2 ms (47.44%) | Memory: 46.8 MB (34.17%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.java
+++ 3194-find-words-containing-character/find-words-containing-character.java
@@ -23,22 +23,26 @@ class Solution {
 
 
     private List<Integer> optimalSolution(String[] words, char x) {
-        return null;
+        List<Integer> result = new ArrayList<Integer>();
+
+        for (int idx = 0; idx < words.length; idx++) {
+            if (words[idx].contains(String.valueOf(x))) {
+                result.add(idx);
+            }
+        }
+
+        return result;
     }
+
+    /*
+        Analysis:
+            Time Complexity  : O(n*m) -> Where n is the number of the words and m is the length of the each word in words array.
+            Space Complexity : O(1)
+    */
 
 
     public List<Integer> findWordsContaining(String[] words, char x) {
-
-        return bruteForceSolution(words, x);
-        // List<Integer> result = new ArrayList<Integer>();
-
-        // for (int idx = 0; idx < words.length; idx++) {
-        //       if (words[idx].indexOf(x) != -1) {
-        //         result.add(idx);
-        //     }
-        // }
-
-        // return result;
+        return optimalSolution(words, x);
     }
 
     /*
