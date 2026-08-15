2c78f9850de67a25e1ef17371605cd0a7e98a147
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2026-08-15 15:41:43 +0530

    Time: 44 ms (35.16%) | Memory: 47 MB (59.64%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,67 +1,48 @@
 class Solution {
 
-    // Brute Force solution check for the each possible pair
-    private int[] bruteForceSolution(int[] nums, int target) {
-        for (int i = 0; i < nums.length; ++i) {
-            for (int j = 0; j < nums.length; ++j) {
-                if (nums[i] + nums[j] == target && i != j) {
-                    return new int[] { i, j };
+    // Solution - 1 : Brute Force solution by checking the each pair without using the same element twice.
+    private int[] solution1(int[] nums, int target) {
+        for (int iIdx = 0; iIdx < nums.length; iIdx++) {
+            for (int jIdx = 0; jIdx < nums.length; jIdx++) {
+
+                // Check the condition
+                if (nums[iIdx] + nums[jIdx] == target && iIdx != jIdx) {
+                    return new int[] {iIdx, jIdx};
                 }
             }
         }
 
-        return new int[] { -1, -1 };
-    }
-    /*
-        Analysis:
-            Time Complexity  : O(n^2)
-            Space Complexity : O(1)
-    */
-
-    // Little bit optimisation
-    private int[] solutionTwo(int[] nums, int target) {
-        for (int i = 0; i < nums.length; ++i) {
-            for (int j = i + 1; j < nums.length; ++j) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[] { i, j };
-                }
-            }
-        }
-        return new int[] {};
+        return new int[] {-1, -1};
     }
 
     /*
         Analysis:
             Time Complexity  : O(n^2)
-            Space Complexity : O(1)
+            Space Coplexity  : O(1)
     */
 
-    // Optimised solution - Store the element in hashMap and check if the element (complement = target - currentElement) exist in the hashMap then return the index of the current element along with the index of complement
-    private int[] optimisedSolution(int[] nums, int target) {
-        Map<Integer, Integer> elementTracker = new HashMap<Integer, Integer>();
+    // Solution - 2 : Instead of starting the jIdx = 0 we can start the jIdx from iIdx + 1.
+    private int[] solution2(int[] nums, int target) {
 
-        for (int i = 0; i < nums.length; ++i) {
-            int complement = target - nums[i];
+        for (int iIdx = 0; iIdx < nums.length; iIdx++) {
+            for (int jIdx = iIdx + 1; jIdx < nums.length; jIdx++) {
 
-            if (elementTracker.containsKey(complement)) {
-                return new int[] { elementTracker.get(complement), i };
+                if (nums[iIdx] + nums[jIdx] == target) {
+                    return new int[] {iIdx, jIdx};
+                }
             }
-
-            // Put the element into the hashmap
-            elementTracker.put(nums[i], i);
         }
 
-        // return the empty array
-        return new int[] {};
+        // If we don't found any pair of elements where sum of two numbers is equal to the target
+        return new int[] {-1, -1};
     }
-
-    /*  
+    /*
         Analysis:
-            Time Complexity  : O(n)
-            Space Complexity : O(n)
+            Time Complexity  : O(n^2)
+            Space Complexity : O(1)
     */
 
     public int[] twoSum(int[] nums, int target) {
-        return optimisedSolution(nums, target);
+        return solution2(nums, target);
     }
 }
