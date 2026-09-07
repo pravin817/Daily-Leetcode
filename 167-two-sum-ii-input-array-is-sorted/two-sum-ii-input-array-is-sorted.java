7b299fb98a59fd94bcbaec6156720d907b7d232d
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2026-09-07 08:39:18 +0530

    Time: 6 ms (12.52%) | Memory: 49.2 MB (5.04%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -6,12 +6,12 @@ class Solution {
         for (int i = 0; i < numbers.length; i++) {
             for (int j = 0; j < numbers.length; j++) {
                 if (numbers[i] + numbers[j] == target && i != j) {
-                    return new int[] {i+1, j+1};
+                    return new int[] { i + 1, j + 1 };
                 }
             }
         }
 
-        return new int[]{};
+        return new int[] {};
     }
 
     /*
@@ -27,14 +27,14 @@ class Solution {
     private int[] twoSumSolution2(int[] numbers, int target) {
 
         for (int i = 0; i < numbers.length; i++) {
-            for (int j = i+1; j < numbers.length; j++) {
+            for (int j = i + 1; j < numbers.length; j++) {
                 if (numbers[i] + numbers[j] == target) {
-                    return new int[] {i+1, j+1};
+                    return new int[] { i + 1, j + 1 };
                 }
             }
         }
 
-        return new int[]{};
+        return new int[] {};
     }
 
     /*
@@ -43,7 +43,44 @@ class Solution {
             Space Complexity : O(1)
     */
 
+    /**
+     * We can further optimised this solution by using the hashMap. We will keep the track of the element and its index.
+    
+        We need to find the indices such the numbers[i] + numbers[j] == target.
+    
+        we can say complement = target - numbers[i];
+    
+        We will find the complement in the hashMap if we get the complement then we will return the index of the complement + 1 and currentIdx + 1
+    */
+
+    private int[] twoSumHashMapBasedSolution(int[] numbers, int target) {
+
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+        for (int idx = 0; idx < numbers.length; idx++) {
+
+            // 1. Find the complement
+            int complement = target - numbers[idx];
+
+            // 2. Find the complement is exist in the map
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement) + 1, idx + 1 };
+            }
+
+            // Add the number to the map
+            map.put(numbers[idx], idx);
+        }
+
+        return new int[] {};
+    }
+
+    /*
+        Analysis:
+            Time Complexity  : O(n)
+            Space Complexity : O(n)
+    */
+
     public int[] twoSum(int[] numbers, int target) {
-        return twoSumSolution2(numbers, target);
+        return twoSumHashMapBasedSolution(numbers, target);
     }
 }
