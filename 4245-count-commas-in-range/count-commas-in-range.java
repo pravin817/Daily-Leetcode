a595efc5b34990190bb815ac544d4ad021659420
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2026-09-09 06:48:13 +0530

    Time: 8 ms (8.36%) | Memory: 42.8 MB (8.36%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.java
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -1,11 +1,26 @@
 class Solution {
-    public int countCommas(int n) {
-        return (n > 999) ? ( n - 999) : 0;
+
+    // Brute Force Solution - Simulation
+    private int bruteForceSolution(int n) {
+        int count = 0;
+
+        for (int num = 1; num <= n; num++) {
+            if (num > 999) {
+                count++;
+            }
+        }
+
+        return count;
     }
 
     /*
         Analysis:
-            Time Complexity  : O(1)
+            Time Complexity  : O(n)
             Space Complexity : O(1)
     */
+
+    public int countCommas(int n) {
+        return bruteForceSolution(n);
+    }
+
 }
