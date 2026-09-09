b607ccc0da61675e7cc29448210b47cf8fa7271e
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2026-09-09 06:52:48 +0530

    Time: 1 ms (83.58%) | Memory: 42.5 MB (72.73%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.java
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -19,8 +19,41 @@ class Solution {
             Space Complexity : O(1)
     */
 
+    /**
+        We can optimsed using the mathematical formula based on the constraint.
+        The value of the n is [1, n] and n <= 1_00_000.
+
+        So the total number of the commas between [1, 999] = 0
+        and in between the [1_000, 1_00_000] = If we have given n then n - 1_000 + 1;
+
+        We can write this as = n - 999
+
+        Hence 
+
+        if n > 999:
+            return n - 999
+        else 
+            return 0
+    
+     */
+
+    private int optimizedSolution(int n) {
+        if (n > 999) {
+            return n - 999;
+        } else {
+            return 0;
+        }
+    }
+
+    /*
+        Analysis:
+            Time Complexity  : O(1)
+            Space Complexity : O(1)
+    */
+
+
     public int countCommas(int n) {
-        return bruteForceSolution(n);
+        return optimizedSolution(n);
     }
 
 }
