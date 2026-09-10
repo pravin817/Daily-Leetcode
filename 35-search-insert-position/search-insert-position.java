36326e3c75280816b23b7664fc9f92bada708625
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2026-09-10 08:37:38 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (82.91%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -27,8 +27,42 @@ class Solution {
             Time Complexity  : O(n)
             Space Complexity : O(1)
     */
+    
+    /**
+        Optimal Solution - Usig Binary Search
+
+        As the array is sorted and elements in the array are distinct then we can use the binary search to find the index of the target if present in the array else the position at which we can put the target element in the array    
+    */
+    private int searchIndexBinarySearch(int[] nums, int target) {
+        int start = 0;
+        int end   = nums.length - 1;
+
+        while (start <= end) {
+    
+            // Find the mid element
+            int mid = start + (end - start) / 2;
+
+            // Return the index of the target elememt
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        // Return the index at which we can insert the target element
+        return start;
+    }
+    /*
+        Analysis:
+            Time Compleixty  : O(log n)
+            Space Complexity : O(1)
+    */
+
 
     public int searchInsert(int[] nums, int target) {
-        return searchIndexLinearSearch(nums, target);
+        return searchIndexBinarySearch(nums, target);
     }
 }
