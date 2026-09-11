059e125025312e36e6de8ebe484d03aeb4d532ab
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2026-09-11 15:35:20 +0530

    Time: 26 ms (48.48%) | Memory: 47 MB (57.56%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -4,6 +4,40 @@ class Solution {
         return s ==  null || s.isEmpty();
     }
 
+
+    // Brute force solution - Find the first index and last index of the character if both same then this is unique character from the string. In string there could be multiple unique character we need to return the firt one.
+
+    private int bruteForceSolution(String s) {
+
+        // If the string is null or empty then return the -1
+        if (isEmpty(s)) {
+            return -1;
+        }
+
+        for (int idx = 0; idx < s.length(); idx++) {   //O(n)
+            char ch = s.charAt(idx);
+
+            int firstIdx = s.indexOf(ch);             // O(n)
+            int lastIdx  = s.lastIndexOf(ch);         // O(n)
+
+            // First unique element (As we are starting from 0th index)
+            if (firstIdx == lastIdx) {
+                return idx;
+            }
+        }
+
+        // If the given string does not contains the unique character
+        return -1;
+    }
+
+    /*
+        Analysis:
+            Time Complexity  : O(n^2)
+            Space Complexity : O(1)
+    */
+
+
+
     // Solution using the Array
     private int solutionUsingArray(String s) {
         if (isEmpty(s)) {
@@ -34,7 +68,7 @@ class Solution {
 
     /*
         Analysis:
-            Time Complexity  : O(2n)   -> O(n)
+            Time Complexity  : O(2n)   -> O(n), Where n is the length of the string
             Space Complexity : O(26)   -> O(1)
     */
 
@@ -65,12 +99,11 @@ class Solution {
 
     /*
         Analysis:
-            Time Complexity  : O(2n)   -> O(n)
+            Time Complexity  : O(2n)   -> O(n) , Where n is the length of the string
             Space Complexity : O(26)   -> O(1)
     */
 
-
     public int firstUniqChar(String s) {
-        return solutionUsingHashMap(s);
+        return bruteForceSolution(s);
     }
 }
