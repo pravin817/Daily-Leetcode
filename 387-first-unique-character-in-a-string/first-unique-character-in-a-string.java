97d737213e3001c37380948485f7cb228e8c7999
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2026-09-11 15:21:24 +0530

    Time: 7 ms (57.97%) | Memory: 47 MB (57.56%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -1,21 +1,34 @@
 class Solution {
-    public int firstUniqChar(String s) {
-        int[] records = new int[26];
 
-        for (int idx = 0; idx < s.length(); idx++) {  // O(n)
-            char ch = s.charAt(idx);
-            records[ch - 'a']++;
+    private boolean isEmpty(String s) {
+        return s ==  null || s.isEmpty();
+    }
+
+    // Solution using the Array
+    private int solutionUsingArray(String s) {
+        if (isEmpty(s)) {
+            return -1;
         }
 
-        // Now traverse the string to find first not repeating character
+        int[] counts = new int[26];
+
         for (int idx = 0; idx < s.length(); idx++) {   // O(n)
             char ch = s.charAt(idx);
 
-            if (records[ch - 'a'] == 1) {
+            // Increase the count of each character
+            counts[ch - 'a']++;
+        }
+
+        // Find the first non repeating character means unique character
+        for (int idx = 0; idx < s.length(); idx++) {   // O(n)
+            char ch = s.charAt(idx);
+
+            if (counts[ch - 'a'] == 1) {
                 return idx;
             }
         }
 
+        // if there is no any unique character then return -1
         return -1;
     }
 
@@ -24,4 +37,9 @@ class Solution {
             Time Complexity  : O(2n)   -> O(n)
             Space Complexity : O(26)   -> O(1)
     */
+
+
+    public int firstUniqChar(String s) {
+        return solutionUsingArray(s);
+    }
 }
