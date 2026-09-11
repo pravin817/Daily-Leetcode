9b0d187e2abd409f2508c57fb5568ee9a8a34d22
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2026-09-11 17:34:10 +0530

    Time: 1 ms (99.86%) | Memory: 46.8 MB (45.66%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.java
+++ 412-fizz-buzz/fizz-buzz.java
@@ -4,7 +4,7 @@ class Solution {
     private List<String> solution(int n) {
         List<String> result = new ArrayList<String>(n);
 
-        for (int num = 1; num <= n; num++) {
+        for (int num = 1; num <= n; num++) {  // O(n)
 
             if (num % 3 == 0 && num % 5 == 0) {
                 result.add("FizzBuzz");
@@ -26,7 +26,45 @@ class Solution {
             Space Complexity : O(1), As the result is needed to return  the solution of the problem
     */
 
+    // Solution 2 - Without using the % (Modulo operator).
+    // We have observed that the pattern is repeating after some time. Like after 3 iterations we have the Fizz and after 3 and 5 iterations we have the FizzBuzz and after 5 iterations we have the Buzz again
+
+    private List<String> solutionWithModuloOperator(int n) {
+        List<String> result = new ArrayList<String>();
+
+        int num = 1, fizz = 0, buzz = 0;
+
+        while(num <= n) {
+            fizz++;
+            buzz++;
+
+            if (fizz == 3 && buzz == 5) {
+                result.add("FizzBuzz");
+                fizz = 0;
+                buzz = 0;
+            } else if (fizz == 3) {
+                result.add("Fizz");
+                fizz = 0;
+            } else if (buzz == 5) {
+                result.add("Buzz");
+                buzz = 0;
+            } else {
+                result.add(String.valueOf(num));
+            }
+
+            num++;
+        }
+
+        return result;
+    }
+
+    /*
+        Analysis:
+            Time Complexity  : O(n)
+            Space Complexity : O(1)
+    */
+
     public List<String> fizzBuzz(int n) {
-        return solution(n);
+        return solutionWithModuloOperator(n);
     }
 }
