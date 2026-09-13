416f61cfad89dfa327bfcc2495c854882dbbaa1c
Pravin Mhaske <83346334+pravin817@users.noreply.github.com>
2026-09-13 23:47:52 +0530

    Time: 0 ms (100.00%) | Memory: 48.5 MB (13.94%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,8 +1,6 @@
 class Solution {
 
-
-    // Brute Force Solution - We will check each element from the array one by one and returns the index where we found that element as there is always unique elements in the array. Here we can use the Linear Search
-
+    // Brute Force solution - We need to find the index of the target element from the array. If the target element is present return it index else return -1
     private int linearSearch(int[] nums, int target) {
         for (int idx = 0; idx < nums.length; idx++) {
             if (nums[idx] == target) {
@@ -12,57 +10,15 @@ class Solution {
 
         return -1;
     }
+
     /*
         Analysis:
             Time Complexity  : O(n)
             Space Complexity : O(1)
     */
 
-
-    // Solution - 2 : As the elements of the array are sorted in the asceding order we can use the binary search to find the target element in the array. If the target element is found in the array then we will return its index else we will return -1 indicating that the target element is not present in the array.
-
+    // Optimal solution - As the array is sorted in the non decreasing order, we can use the binary search.
     private int binarySearch(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-
-        while (left <= right) {
-            int mid = (left + right) / 2;
-
-            if (nums[mid] == target) {
-                return mid;
-            } else if(nums[mid] > target) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-
-        return -1;
-    }
-
-    /*
-        Analyis:
-            Time Complexity  : O(logn)
-            Space Complexity : O(1)
-    */
-
-
-    // But here is small bug in the formula for the finding the mid of the array
-    // The formula that we have used above was: 
-    //    int mid = (left + right ) / 2;
-    // What If the value of left and right is near equal to the Integer.MAX_VALUE in this case it will be overflow and gaves us negative value.
-    // So inorder to resolve this issue we can use the same formula by modifying it as below
-    // int mid = left + (right - left)
-    //                  --------------
-    //                         2
-    // int mid = 2 * left + right - left
-    //          -------------------------
-    //                     2
-    //         = left + right
-    //          --------------
-    //                2
-
-    private int binarySearchImproved(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
 
@@ -71,13 +27,14 @@ class Solution {
 
             if (nums[mid] == target) {
                 return mid;
-            } else if (nums[mid] > target) {
-                right = mid - 1;
-            } else {
+            } else if (nums[mid] < target) {
                 left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
 
+        // If the target element is not found in the array then return -1
         return -1;
     }
     /*
@@ -87,6 +44,6 @@ class Solution {
     */
 
     public int search(int[] nums, int target) {
-        return binarySearchImproved(nums, target);
+        return binarySearch(nums, target);
     }
 }
